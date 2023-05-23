d02852a73f0233ec1e920d701fcc3739b35bfa63
yarikpo <yaroslavpopovich04@gmail.com>
2023-05-24 02:01:20 +0300

    Frontend: DONE: Added SideBar and routes such as products, login, user info, chat, track, faq. INFO: TODO routes

--- client/src/app/containers/App.jsx
+++ client/src/app/containers/App.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    useNavigate
+} from 'react-router-dom';
+import * as PAGES from 'constants/pages';
+import '../styles/App.css';
+import SideBar from "components/SideBar/SideBar";
+
+
+const HomePage = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        navigate(`/${PAGES.PRODUCTS}`);
+    }, [navigate]);
+
+    return null;
+};
 
 const App = () => {
 
     return (
-        <h1>Hello, World!</h1>
+        <div className="App">
+            <Router>
+                <SideBar />
+                {/* SIdebar */}
+                <Routes>
+                    <Route path="*" element={<HomePage />} />
+                    <Route path={`/${PAGES.PRODUCTS}`} element={<h1>PRODUCTS</h1>} />
+                    <Route path={`/${PAGES.LOGIN}`} element={<h1>LOGIN</h1>} />
+                    <Route path={`/${PAGES.USER_INFO}`} element={<h1>USER_INFO</h1>} />
+                    <Route path={`/${PAGES.CHAT}`} element={<h1>CHAT</h1>} />
+                    <Route path={`/${PAGES.TRACK}`} element={<h1>TRACK</h1>} />
+                    <Route path={`/${PAGES.FAQ}`} element={<h1>FAQ</h1>} />
+                </Routes>
+            </Router>
+        </div>
     );
 };
 
