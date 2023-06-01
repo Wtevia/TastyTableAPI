67a29e68b6a03310832d19e7309280960db8eabf
matvii <wteviakh@gmail.com>
2023-06-02 02:04:04 +0300

    feat: added correct relation between projects, setup Program.cs

--- TastyTableAPI/Program.cs
+++ TastyTableAPI/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
+BLL.DependencyRegistrar.ConfigureServices(builder.Services);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,8 +20,15 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+//app.UseRouting();
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+// app.UseEndpoints(endpoints =>
+// {
+//     endpoints.MapControllers();
+// });
 
 app.Run();
