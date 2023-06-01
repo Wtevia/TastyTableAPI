57897d8a6b50895a5bfacb81b8c141ce6e4c5d22
matvii <wteviakh@gmail.com>
2023-06-02 02:52:12 +0300

    feat: created Restaurant DbContext

--- DAL/DependencyRegistrar.cs
+++ DAL/DependencyRegistrar.cs
@@ -1,7 +1,6 @@
 using System.IO;
 using DAL.Contexts;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DAL
@@ -10,19 +9,10 @@ namespace DAL
     {
         public static void ConfigureServices(IServiceCollection services)
         {
-            var configuration = new ConfigurationBuilder()
-                    .SetBasePath(/*"B:\\NIXTraining\\Messenger\\Application\\PresentationLayer"*/Directory.GetCurrentDirectory())
-                    .AddJsonFile("appsettings.json", false)
-                    .AddEnvironmentVariables()
-                    .Build();
-
-            services.AddTransient<Core.AppSettings>();
-            services.Configure<Core.AppSettings>(configuration.GetSection("AppSettings"));
-            services.AddScoped(builder => configuration.GetSection("AppSettings").Get<Core.AppSettings>());
-
             var connectionString = services.BuildServiceProvider()
                 .GetRequiredService<Core.AppSettings>().DefaultConnection;
-            services.AddDbContext<MessengerContext>(
+            
+            services.AddDbContext<RestaurantContext>(
                 builder => builder.UseMySql(connectionString , ServerVersion.AutoDetect(connectionString)),
                 ServiceLifetime.Transient);
         }
