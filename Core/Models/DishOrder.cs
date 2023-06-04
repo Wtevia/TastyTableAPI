9d9635ce4217e227ad726247d0da7f73d2298a75
matvii <wteviakh@gmail.com>
2023-06-04 23:26:50 +0300

    feat: created Dish and Order related Models

---
+++ Core/Models/DishOrder.cs
@@ -0,0 +1,19 @@
+﻿namespace Core.Models;
+
+public class DishOrder : BaseEntity
+{
+    public int OrderId { get; set; }
+    
+    public int DishId { get; set; }
+    
+    public virtual Order Order { get; set; }
+    
+    public virtual Dish Dish { get; set; }
+
+    public int Count { get; set; }
+
+    public float GetPrice()
+    {
+        return this.Count * this.Dish.Price;
+    }
+}
