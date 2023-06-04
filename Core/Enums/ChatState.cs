8f4e860bdedbd562b29b15a63197918884f5ba51
matvii <wteviakh@gmail.com>
2023-06-04 23:24:58 +0300

    feat: created chat related models

---
+++ Core/Enums/ChatState.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Enums;
+
+public enum ChatState
+{
+    Open, // When user requests chat with Support team
+    InProgress, // Chat in progress
+    Closed // Conversation is closed
+}
---
+++ Core/Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Core.Models;
+
+public class Message : BaseEntity
+{
+    public string Text { get; set; }
+    
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    
+    public virtual Chat Chat { get; set; }
+}
