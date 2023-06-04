c0435a7bbd846143ec95e285930962d7a902e368
matvii <wteviakh@gmail.com>
2023-06-04 23:27:31 +0300

    ref: setup RestaurantDbContext

--- DAL/Contexts/RestaurantContext.cs
+++ DAL/Contexts/RestaurantContext.cs
@@ -1,3 +1,4 @@
+using System.Security.AccessControl;
 using Core.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,11 +6,40 @@ namespace DAL.Contexts
 {
     public sealed class RestaurantContext : DbContext
     {
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Chat> Chats { get; set; } = null!;
+        public DbSet<ChatUser> ChatUsers { get; set; } = null!;
+        public DbSet<Message> Messages { get; set; } = null!;
+        
+        public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<Dish> Dishes { get; set; } = null!;
+        public DbSet<DishOrder> DishOrders { get; set; } = null!;
+        
         
         public RestaurantContext(DbContextOptions<RestaurantContext> options)
         : base(options)
         {
+            Database.EnsureDeleted();
+            Console.WriteLine("RestaurantContext Constructor");
             Database.EnsureCreated();
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Console.WriteLine("DbContext OnModelCreating");
+            modelBuilder.Entity<ChatUser>()
+                .HasAlternateKey(uc => new { uc.UserId, uc.ChatId });
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Chats)
+                .WithMany(c => c.Users)
+                .UsingEntity<ChatUser>();
+
+            modelBuilder.Entity<DishOrder>()
+                .HasAlternateKey(od => new { od.OrderId, od.DishId });
+            modelBuilder.Entity<Dish>()
+                .HasMany(d => d.Orders)
+                .WithMany(o => o.Dishes)
+                .UsingEntity<DishOrder>();
         }
     }
 }
