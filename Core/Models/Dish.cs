e87246a2a73dd3e83dec2814eb3e4ef8b744a0df
matvii <wteviakh@gmail.com>
2023-06-04 23:26:50 +0300

    feat: created Dish and Order related Models

---
+++ Core/Models/Dish.cs
@@ -0,0 +1,22 @@
+﻿using Core.Enums;
+
+namespace Core.Models;
+
+public class Dish : BaseEntity
+{
+    public string Name { get; set; }
+    
+    public string Description { get; set; }
+    
+    public float Price { get; set; }
+    
+    public string Image { get; set; }
+    
+    public bool IsAvailable { get; set; }
+    
+    public DishCategory Category { get; set; }
+    
+    public virtual List<DishOrder> OrderDishes { get; } = new();
+    
+    public virtual List<Order> Orders { get; } = new();
+}
