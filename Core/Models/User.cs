2c9ce70899b1010207ac3f37a067914e4af93d90
matvii <wteviakh@gmail.com>
2023-06-04 23:24:40 +0300

    feat: added User related models

---
+++ Core/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using Core.Enums;
+
+namespace Core.Models;
+
+public class User : BaseEntity
+{
+    public string Name { get; set; }
+
+    public string Email { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string Password { get; set; }
+
+    public DateTime DateOfBirth { get; set; }
+
+    public string Address { get; set; }
+
+    public UserRole UserRole { get; set; }
+
+    public virtual List<Order> Orders { get; } = new();
+
+    public virtual List<ChatUser> UserChats { get; } = new();
+
+    public virtual List<Chat> Chats { get; } = new();
+}
