31ddd48c406935c7427be41e944e2453a13feafb
matvii <wteviakh@gmail.com>
2023-06-04 23:27:49 +0300

    feat: created IUserService with realisation

---
+++ BLL/Services/UserService.cs
@@ -0,0 +1,25 @@
+﻿using BLL.Interfaces;
+using Core.Models;
+using DAL.Contexts;
+
+namespace BLL.Services;
+
+public class UserService : IUserService
+{
+    private RestaurantContext _dbContext;
+
+    public UserService(RestaurantContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public User GetUserById(int id)
+    {
+        return _dbContext.Users.Find(id) ?? throw new NullReferenceException();
+    }
+    
+    public IQueryable<User> GetUsers(int count)
+    {
+        return _dbContext.Users.Take(count);
+    }
+}
