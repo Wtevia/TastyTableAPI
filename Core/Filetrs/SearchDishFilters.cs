ceb94f73e55d160638c16396a58f729d7eb69baf
matvii <wteviakh@gmail.com>
2023-06-13 04:51:14 +0300

    feat: created DishController and SearchDishFilters model

---
+++ Core/Filetrs/SearchDishFilters.cs
@@ -0,0 +1,11 @@
+﻿using Core.Enums;
+
+namespace TastyTableAPI;
+
+public class SearchDishFilters
+{
+    public string Substring { get; set; } = "";
+    public string? DishCategory { get; set; } = null;
+    public int MinPrice { get; set; } = 0;
+    public int MaxPrice { get; set; } = int.MaxValue;
+}
