ddeca8cdd4359cda183bcec1bfb9980a9ccad694
matvii <wteviakh@gmail.com>
2023-06-13 03:22:34 +0300

    ref: fully refactored IAuthService and its implementation

--- BLL/Interfaces/IAuthService.cs
+++ BLL/Interfaces/IAuthService.cs
@@ -1,13 +1,22 @@
-﻿using Core.CredentialModels;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Core.CredentialModels;
 using Core.Models;
+using FluentResults;
+using Google.Apis.Auth;
+using Microsoft.AspNetCore.Identity;
 
 namespace BLL.Interfaces;
 
 public interface IAuthService
 {
-    JwtTokens GenerateJwtTokens(User user);
+    Task<JwtTokens> GenerateJwtTokens(User user);
 
-    User? LogIn(LogInModel logInModel);
-    
-    User RegisterUser(RegisterUserModel logInModel);
+    Task<User> PasswordLogIn(LogInPassword logInPassword);
+
+    Task<User> RegisterUser(RegisterUserModel registerModel);
+
+    Task<User> SocialLoginOrRegister(SocialLogin socialLogin, SocialTokenValidationResult validationResult);
+
+    Task<Result<SocialTokenValidationResult>> ValidateSocialToken(SocialLogin request);
 }
